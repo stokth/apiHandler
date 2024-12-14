73f2e957cb795975b883ee591fa3fd26fe2aa036
Sergey Shvchenko <stokth.geek@gmail.com>
2024-12-14 18:14:21 +0300

    Код по уроку Жени

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var counter int
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Fprintln(w, "Counter = ", strconv.Itoa(counter))
+	} else {
+		fmt.Fprintln(w, "Method not allowed")
+	}
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		counter++
+		fmt.Fprintln(w, "Counter incremented")
+	} else {
+		fmt.Fprintln(w, "Method not allowed")
+	}
+}
+
+func main() {
+	http.HandleFunc("/get", GetHandler)
+	http.HandleFunc("/post", PostHandler)
+	http.ListenAndServe("localhost:8080", nil)
+}
